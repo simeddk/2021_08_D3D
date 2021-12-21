3119ae1ce2f9e50b5d610d8f0c6843f7e93a16f3
simeddk <65766852+simeddk@users.noreply.github.com>
2021-12-21 14:52:21 +0900

    21_Animation1

--- D3D/ModelEditor/Types.h
+++ D3D/ModelEditor/Types.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "stdafx.h"
 
+//-----------------------------------------------------------------------------
+//Mesh
+//-----------------------------------------------------------------------------
 struct asBone
 {
 	int Index;
@@ -43,3 +46,64 @@ struct asMaterial
 	string SpecularFile;
 	string NormalFile;
 };
+
+//-----------------------------------------------------------------------------
+//Skinning
+//-----------------------------------------------------------------------------
+struct asBlendWeight
+{
+	Vector4 Indices = Vector4(0, 0, 0, 0);
+	Vector4 Weights = Vector4(0, 0, 0, 0);
+
+	void Set(UINT index, UINT boneIndex, float weight)
+	{
+		float i = (float)boneIndex;
+		float w = weight;
+
+		switch (index)
+		{
+			case 0: Indices.x = i; Weights.x = w; break;
+			case 1: Indices.y = i; Weights.y = w; break;
+			case 2: Indices.z = i; Weights.z = w; break;
+			case 3: Indices.w = i; Weights.w = w; break;
+		}
+	}
+};
+
+struct asBoneWeights
+{
+private:
+	typedef pair<int, float> Pair;
+	vector<Pair> BoneWeights;
+
+public:
+	void AddWeights(UINT boneIndex, float boneWeights)
+	{
+		if (boneWeights <= 0.0f) return;
+
+		bool bAdd = false;
+
+		vector<Pair>::iterator it = BoneWeights.begin();
+		while (it != BoneWeights.end())
+		{
+			if (boneWeights > it->second)
+			{
+				BoneWeights.insert(it, Pair(boneIndex, boneWeights));
+				bAdd = true;
+
+				break;
+			}
+
+			it++;
+		}
+
+		if (bAdd == false)
+			BoneWeights.push_back(Pair(boneIndex, boneWeights));
+	}
+
+	//TODO
+};
+
+//-----------------------------------------------------------------------------
+//Animation
+//-----------------------------------------------------------------------------

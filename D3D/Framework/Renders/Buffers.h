20eb8c197380b5f9685118950761747487045e9e
simeddk <65766852+simeddk@users.noreply.github.com>
2022-01-04 12:51:42 +0900

    25_TextureBuffer_Middle

--- D3D/Framework/Renders/Buffers.h
+++ D3D/Framework/Renders/Buffers.h
@@ -123,3 +123,38 @@ private:
 	UINT inputByte;
 	UINT outputByte;
 };
+
+//-----------------------------------------------------------------------------
+//TextureBuffer
+//-----------------------------------------------------------------------------
+class TextureBuffer : public CsReosource
+{
+public:
+	TextureBuffer(ID3D11Texture2D* src);
+	~TextureBuffer();
+
+private:
+	void CreateSRV() override;
+
+	void CreateOutput() override;
+	void CreateUAV() override;
+
+
+public:
+	UINT Width() { return width; }
+	UINT Height() { return height; }
+	UINT ArraySize() { return arraySize; }
+
+	ID3D11ShaderResourceView* OutputSRV() { return outputSRV; }
+	ID3D11Texture2D* Result() { return result; }
+
+	void CopyToInput(ID3D11Texture2D* texture);
+	ID3D11Texture2D* CopyFromOutput();
+
+private:
+	UINT width, height, arraySize;
+	DXGI_FORMAT format;
+
+	ID3D11ShaderResourceView* outputSRV;
+	ID3D11Texture2D* result = nullptr;
+};
